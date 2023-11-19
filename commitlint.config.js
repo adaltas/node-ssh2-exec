39cc448b621914f1c942d89ab34578fc954627d4
David Worms <david@adaltas.com>
2023-11-19 14:36:49 +0100

    build: commitlint types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,20 @@
 module.exports = {
-  extends: [
-    "@commitlint/config-conventional"
-  ]
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "refactor",
+        "revert",
+        "test",
+      ],
+    ],
+  },
 };
